977b4451218473f4c5bb9aaf5cc7dd63d29a85fd
Sampat Panaskar <sampat.panaskar@otsconsultingservices.com>
2023-06-29 00:27:53 +0530

    done the design.

--- app-increment-decrement/src/App.js
+++ app-increment-decrement/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Card>
+        <Card.Header>Increment/Decrement Counter</Card.Header>
+        <Card.Body>
+          <Card.Text>Using React and Redux</Card.Text>
+          <ButtonGroup aria-label="Basic example">
+            <Button variant="danger">-</Button>
+            <input readOnly value={0}></input>
+            <Button variant="success">+</Button>
+          </ButtonGroup>
+        </Card.Body>
+      </Card>
+      
+    </>
   );
 }
 
